b49b29b081493d721e68493c335f4225938863cf
Prakash Loganathan <47576906+prakloga@users.noreply.github.com>
2025-03-10 10:50:42 -0500

    Update streamlit_app.py
    added logic to check pandas column value matching

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit
 
 # Convert Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -53,6 +53,11 @@ if ingredients_list:
     # Use FOR loop to convert LIST to String
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' ' # Add space after each fruit name
+        # You can read more about loc and iloc functions here: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
