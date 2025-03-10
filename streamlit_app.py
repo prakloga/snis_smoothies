003237020468e5bff386175f7d1b8ea3e10e38b7
Prakash Loganathan <47576906+prakloga@users.noreply.github.com>
2025-03-10 10:35:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Display fruit options list in Streamlit in Snowflake(SiS)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_name"), col("search_on")
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_name"), col("search_on"))
 st.stop()
 
 ingredients_list = st.multiselect(
