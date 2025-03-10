283647e41e0918754e54f9a116c7fb22f3f45f83
Prakash Loganathan <47576906+prakloga@users.noreply.github.com>
2025-03-10 10:09:52 -0500

    Update streamlit_app.py
    Our fruit chosen variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,9 @@ if ingredients_list:
     # Use FOR loop to convert LIST to String
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' ' # Add space after each fruit name
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     # Output the string
