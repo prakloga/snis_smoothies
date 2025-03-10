ace97e245c37a2382b055d4818e383027d9ac304
Prakash Loganathan <47576906+prakloga@users.noreply.github.com>
2025-03-10 10:54:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,12 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' ' # Add space after each fruit name
         # You can read more about loc and iloc functions here: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     # Output the string
